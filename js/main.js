61fddc4541795817ff7a3b6b2d71fe68c1a6fb9c
SlientKnight <metal.alcantara76@gmail.com>
2021-10-25 03:52:44 -0300

    pagina html com js

---
+++ js/main.js
@@ -0,0 +1,51 @@
+var d = new Date()
+alert(d.getDay())
+
+/*
+var count;
+for (count=0; count<=5; count++) {
+    alert(count)
+}
+*/
+
+/*
+var count = 0;
+while (count<=5) {
+    console.log(count);
+    count ++;
+}
+*/
+
+/*
+var idade = prompt("Qual a sua idade maninho? ");
+if (idade >= 18) {
+    alert("parabens ja pode ser preso")
+} else {
+    alert("menor de idade, não foi dessa vez")
+} 
+*/
+
+/*
+var frutas = [{nome:"maça",cor: "vermelho"},{nome: "uva", cor: "roxa"}]
+console.log(frutas)
+alert(frutas[0].cor)
+*/
+
+/*
+var lista = ["maça","pera","laranja"]
+lista.push("uva")
+lista.pop("uva")
+console.log(lista.length)
+*/
+
+/*
+var nome = "Pedro Alcântara";
+var n1 = 5 ;
+var n2 = 4 ;
+console.log(n1 * n2) ;
+var frase = "o japão tem o melhor time do mundo" ;
+alert("bem vindo " + nome) 
+frase.replace("japão","brasil");
+alert (frase.toUpperCase());
+alert("seja bem viado " + nome);
+*/
